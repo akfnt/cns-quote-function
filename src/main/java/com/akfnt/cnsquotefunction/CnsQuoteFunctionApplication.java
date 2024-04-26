51925ae4adbc504cb2130dfd0dd13c07ef011991
akfnt <akfnt@naver.com>
2024-04-26 14:07:03 +0900

    init project

---
+++ src/main/java/com/akfnt/cnsquotefunction/CnsQuoteFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.akfnt.cnsquotefunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsQuoteFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsQuoteFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akfnt/cnsquotefunction/CnsQuoteFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akfnt.cnsquotefunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsQuoteFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
